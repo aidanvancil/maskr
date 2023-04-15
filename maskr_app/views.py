691347334a9927b83868d410e74102662aaba055
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2023-04-15 16:08:35 -0700

    Swapping Models To OpenAI Variation

--- maskr_app/views.py
+++ maskr_app/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render, redirect, reverse
-from maskr_app.models import Images
-from django.http import HttpResponse
 from django.views.generic import View
-from django.core.files import File
 from .forms import ImgForm
-from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
+import openai
+import environ
+import os
+from PIL import Image
 
+env = environ.Env()
+environ.Env.read_env()
 
-checkpoint = "model.pth"
-model_type = "vit_h"
-
-sam = sam_model_registry[model_type](checkpoint=checkpoint)
-mask_generator = SamAutomaticMaskGenerator(sam)
-
+openai.api_key = env("OPENAI_KEY")
 class maskrView(View):
     def __init__(self, *args, **kwargs):
         super(maskrView, self).__init__(*args, **kwargs)
@@ -26,38 +20,43 @@ class maskrView(View):
     def post(self, request):
         form = ImgForm(request.POST, request.FILES)
         if form.is_valid():
+            prompt_t = request.POST.get('prompt')
+            image_t = request.POST.get('image')
             form.save()
             image_obj = form.instance
-            image = cv2.imread(image_obj.image.path)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            masks = mask_generator.generate(image)
-            
-            plt.figure(figsize=(20,20))
-            plt.imshow(image)
-            #self.show_anns(masks)
-            plt.axis('off')
-            plt.savefig('placeholder.png')
-            with open('placeholder.png', 'rb') as f:
-                print(dir(File(f)))
-                return render(request, 'maskrView.html', {'form': form, 'uploaded': image_obj, 'processed': File(f)})
-            return render(request, 'maskrView.html', {'form': form, 'uploaded': image_obj})
+            img_resized = Image.open(image_obj.image.path)
+            IMG_DIM = (256, 256)
+            img_resized = img_resized.resize(IMG_DIM)
+            img_resized.save("demo.png", "")
+            print(image_obj.image.url)
+            resp = None
+            print(image_t)
+            print(prompt_t)
+            if prompt_t.isalpha() and not image_obj.image.url:
+                resp = openai.Image.create(
+                        prompt=prompt_t,
+                        n=1,
+                        size="256x256"
+                )
+                image_url = resp['data'][0]['url']
+                return render(request, 'maskrView.html', {'form': form, 'processed': image_url})
+            elif prompt_t and image_t:
+                resp = openai.Image.create_edit( 
+                    image=open("demo.png", "rb"), 
+                    prompt=prompt_t, 
+                    n=1, 
+                    size="256x256"
+                )
+                image_url = resp['data'][0]['url']
+                return render(request, 'maskrView.html', {'form': form, 'uploaded': image_obj.image.url, 'processed': image_url})
+            else:
+                """resp = openai.Image.create_variation(
+                    image=open("demo.png", "rb"),
+                    n=1,
+                    size="256x256"
+                )
+                image_url = resp['data'][0]['url']"""
+            return render(request, 'maskrView.html', {'form': form, 'uploaded': image_obj.image.url, 'processed': image_obj.image.url})
         else:
             form = ImgForm()
         return redirect(reverse(''))
-    
-    def show_anns(self, anns):
-        if len(anns) == 0:
-            return
-        sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-        ax = plt.gca()
-        ax.set_autoscale_on(False)
-        polygons = []
-        color = []
-        for ann in sorted_anns:
-            m = ann['segmentation']
-            img = np.ones((m.shape[0], m.shape[1], 3))
-            color_mask = np.random.random((1, 3)).tolist()[0]
-            for i in range(3):
-                img[:,:,i] = color_mask[i]
-            ax.imshow(np.dstack((img, m*0.35)))
-        return 
